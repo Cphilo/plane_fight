584c258f44024138bbba7a1a5cc01f916eec25b8
chengliangliang <cllsince1994@gmail.com>
2013-09-26 12:42:09 +0800

    add mongo, basic style

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 planeFight
 ==========
 
-软件工程课html5打飞机
+Environment:node.js express.js socket.io crafty.js
+
+This is a multiple-players html5 game about the old plane fights.
--- http_server.js
+++ http_server.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var path = require('path');
 
 var app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
+app.use(express.static(path.join(__dirname, "/public")));
 
+var MongoClient = require('mongodb').MongoClient;
 server.listen(8005);
 
+
+MongoClient.connect("mongodb://localhost:27017/planeFight", function(err, db) {
+  if(err) { return console.dir(err); }
+  console.log('connect to database.');
+  db.collection('socket_users', function(err, collection) {
+    if(err)throw err;
+  });
+});
+
 app.get('/', function(req, res) {
-    res.sendfile('templates/index.html');
-    //res.send('Hello');
+    res.sendfile(path.join(__dirname, '/public/templates/index.html'));
 });
 
 io.sockets.on('connection', function(socket) {
