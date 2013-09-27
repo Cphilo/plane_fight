110ad7dcb21fe4d70d04741c4fd6e383fd2d821c
chengliangliang <cllsince1994@gmail.com>
2013-09-27 23:55:57 +0800

    try crafty

--- http_server.js
+++ http_server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-
 var app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+server = require('http').createServer(app),
+io = require('socket.io').listen(server);
 app.use(express.static(path.join(__dirname, "/public")));
 
 var MongoClient = require('mongodb').MongoClient;
@@ -11,21 +10,21 @@ server.listen(8005);
 
 
 MongoClient.connect("mongodb://localhost:27017/planeFight", function(err, db) {
-  if(err) { return console.dir(err); }
-  console.log('connect to database.');
-  db.collection('socket_users', function(err, collection) {
-    if(err)throw err;
-  });
+    if(err) { return console.dir(err); }
+	console.log('connect to database.');
+	db.collection('socket_users', function(err, collection) {
+	    if(err)throw err;
+	});
 });
 
 app.get('/', function(req, res) {
-    res.sendfile(path.join(__dirname, '/public/templates/index.html'));
+	res.sendfile(path.join(__dirname, '/public/templates/index.html'));
 });
 
 io.sockets.on('connection', function(socket) {
-    console.log('connected');
-    socket.emit('news', {hello:'world'});
-    socket.on('my other event', function(data) {
-        console.log(data);
-    });
+	console.log('connected');
+	socket.emit('news', {hello:'world'});
+	socket.on('my other event', function(data) {
+		console.log(data);
+	});
 });
