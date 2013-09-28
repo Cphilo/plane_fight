d784bad2ed999d8c3dc3c946f296be7f67463594
chengliangliang <cllsince1994@gmail.com>
2013-09-28 21:44:27 +0800

    add game create

--- http_server.js
+++ http_server.js
@@ -1,30 +1,51 @@
 var express = require('express');
 var path = require('path');
-var app = express(),
-server = require('http').createServer(app),
-io = require('socket.io').listen(server);
-app.use(express.static(path.join(__dirname, "/public")));
-
+var fs = require('fs');
+var format = require('util').format;
 var MongoClient = require('mongodb').MongoClient;
+
+var app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 server.listen(8005);
+app.use(express.static(path.join(__dirname, "/public")));
+app.configure('production', function(){
+    var logFile = fs.createWriteStream('planeFight.log', {flags: 'a'});
+    app.use(express.logger({stream:logFile}));
+});
+app.configure('development', function(){
+    app.use(express.logger());
+});
 
-
+var mydb;
 MongoClient.connect("mongodb://localhost:27017/planeFight", function(err, db) {
-    if(err) { return console.dir(err); }
-	console.log('connect to database.');
-	db.collection('socket_users', function(err, collection) {
-	    if(err)throw err;
-	});
+    if(err) { console.log(format('db error:%s', err)); }
+    mydb = db;
 });
-
 app.get('/', function(req, res) {
-	res.sendfile(path.join(__dirname, '/public/templates/index.html'));
+    res.sendfile(path.join(__dirname, '/public/templates/index.html'));
 });
-
-io.sockets.on('connection', function(socket) {
-	console.log('connected');
-	socket.emit('news', {hello:'world'});
-	socket.on('my other event', function(data) {
-		console.log(data);
-	});
+app.get('/join/:roomid/:playerid?', function(req, res) {
+    var roomid = req.params.roomid;
+    var playerid = req.params.playerid;
+    res.sendfile(path.join(__dirname, '/public/templates/player.html'));
 });
+var room = io
+    .of('/room')
+    .on('connection', function (socket) {
+        socket.on('createRoom', function(roomid) {
+            mydb.collection('rooms').insert({'roomid':roomid, 'players':[]}, {safe:true}, function(err, records) {
+                if(err){ console.log(format('insert error:%s', err)); }
+            });
+            socket.roomid = roomid;
+            socket.emit('addRoom', roomid);
+            console.log(format('create room %s.', roomid));
+        });
+    });
+var player = io
+    .of('/player')
+    .on('connection', function (socket) {
+        socket.on('addPlayer', function(roomid, playerid) {
+            socket.playerid = playerid;
+        });
+    });
